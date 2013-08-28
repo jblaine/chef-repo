ecf28276cd38858d3ed1dfe6f37ffc80d30517cd
Jeff Blaine <jblaine@kickflop.net>
2013-08-28 15:16:55 -0400

    Import

---
+++ roles/unix-base.rb
@@ -0,0 +1,11 @@
+name "unix-base"
+
+description "Shut up"
+
+run_list "recipe[jblaine-users]"
+
+# Attributes applied if the node doesn't have it set already.
+#default_attributes()
+
+# Attributes applied no matter what the node has set already.
+#override_attributes()
