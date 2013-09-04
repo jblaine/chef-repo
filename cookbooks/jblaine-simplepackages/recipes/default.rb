e6d50137a3ecc7e46a258da1e4f857b4ddaf4367
Jeff Blaine <jblaine@kickflop.net>
2013-09-04 19:11:47 -0400

    Install python, vim

--- cookbooks/jblaine-simplepackages/recipes/default.rb
+++ cookbooks/jblaine-simplepackages/recipes/default.rb
@@ -1,16 +1,25 @@
 return unless platform_family?("rhel")
 
-package 'openssl-devel'
-package 'libyaml-devel'
-package 'nc'
 package 'xterm'
+
 package 'net-snmp-utils'
 package 'telnet'
 package 'strace'
-package 'pylint'
+package 'nc'
+
+package 'openssl-devel'
+package 'libyaml-devel'
 package 'libxml2'
 package 'libxml2-devel'
 package 'libxslt-devel'
 package 'libxslt'
-package 'httpd'
 package 'ncurses-devel'
+
+package 'httpd'
+
+package 'pylint'
+package 'python'
+
+package 'ruby'
+
+package 'vim'
