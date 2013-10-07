0d8663a7348d6735db38c714a9e7e1e53aba53b4
Jeff Blaine <jblaine@kickflop.net>
2013-10-07 13:05:08 -0400

    sdfdasf

--- roles/unix-base.rb
+++ roles/unix-base.rb
@@ -3,7 +3,8 @@ name "unix-base"
 description "Shut up"
 
 run_list "recipe[syslog_handler]",
-         "recipe[jblaine-users]"
+         "recipe[jblaine-users]",
+         "recipe[ntp]"
 
 # Attributes applied if the node doesn't have it set already.
 #default_attributes()
