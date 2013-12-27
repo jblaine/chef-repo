5a13e3a29d1fe8e06b05637042246776ddfeac56
Jeff Blaine <jblaine@kickfop.net>
2013-12-27 17:47:16 -0500

    Stuff

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -2,9 +2,9 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "jblaine"
-client_key               "#{current_dir}/jblaine.pem"
+client_key               "#{ENV['HOME']}/.chef/jblaine.pem"
 validation_client_name   "jblaine-validator"
-validation_key           "/home/jblaine/.chef/jblaine-validator.pem"
+validation_key           "#{ENV['HOME']}/.chef/jblaine-validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/jblaine"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
---
+++ cookbooks/jblaine-packagemgr/recipes/default.rb
@@ -0,0 +1,7 @@
+case node['platform_family']
+when 'rhel'
+  include_recipe "yum"
+  include_recipe "yum::epel"
+when 'debian'
+  include_recipe "apt"
+end
