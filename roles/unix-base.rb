7e6ef9728dbb2e2dde7a1640a9c8079040cd35e9
Jeff Blaine <jblaine@kickflop.net>
2014-08-25 19:15:08 -0400

    Add build-essentials and jblaine-simplepackages

--- roles/unix-base.rb
+++ roles/unix-base.rb
@@ -9,6 +9,8 @@ run_list "recipe[jblaine-chef-client]",
          "recipe[python]",
          "recipe[jblaine-git]",
          "recipe[jblaine-terminfo]",
+         "recipe[build-essential]",
+         "recipe[jblaine-simplepackages]",
          "recipe[ntp]"
 
 # Attributes applied if the node doesn't have it set already.
