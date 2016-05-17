cf8eda1db1a0b12f6d303f4a851ebe8a2362049f
Jeff Blaine <jblaine@kickflop.net>
2016-05-17 08:55:29 -0400

    Import chef-client version 4.5.0

--- cookbooks/chef-client/recipes/bluepill_service.rb
+++ cookbooks/chef-client/recipes/bluepill_service.rb
@@ -9,7 +9,7 @@ Chef::Log.debug("Found chef-client in #{client_bin}")
 node.default['chef_client']['bin'] = client_bin
 create_directories
 
-group = root_group
+group = node['root_group']
 
 directory node['chef_client']['run_path'] do
   recursive true
@@ -18,7 +18,7 @@ directory node['chef_client']['run_path'] do
   mode 0755
 end
 
-include_recipe 'bluepill' # ~FC007: bluepill is only required when using the bluepill_service recipe 
+include_recipe 'bluepill' # ~FC007: bluepill is only required when using the bluepill_service recipe
 
 template "#{node['bluepill']['conf_dir']}/chef-client.pill" do
   source 'chef-client.pill.erb'
